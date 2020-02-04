0e3d244b84c3a3f38e4a643c6bf4b5addd5aff1a
hilkojj <hilkojj@outlook.com>
2020-02-04 15:41:39 +0100

    debug line renderer 2d functions

--- source/graphics/3d/debug_line_renderer.h
+++ source/graphics/3d/debug_line_renderer.h
@@ -21,8 +21,12 @@ class DebugLineRenderer
     // make sure to set 'projection' before calling this.
     void line(const vec3 &p0, const vec3 &p1, const vec3 &color);
 
+    inline void line(const vec2 &p0, const vec2 &p1, const vec3 &color) { line(vec3(p0, 0), vec3(p1, 0), color); }
+
     // make sure to set 'projection' before calling this.
     void axes(const vec3 &p, float size, const vec3 &color);
+
+    inline void axes(const vec2 &p, float size, const vec3 &color) { axes(vec3(p, 0), size, color); }
 
     void square(const vec3 &p, float size, const vec3 &color, const vec3 &xAxis=mu::X, const vec3 &yAxis=mu::Y);
 
