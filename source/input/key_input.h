786fe861fb30d1063ccc76f1c6c67c08ccd904ff
hilkojj <hilkojj@outlook.com>
2020-07-06 22:33:10 +0200

    readable key names and stuff

--- source/input/key_input.h
+++ source/input/key_input.h
@@ -3,8 +3,10 @@
 #define KEY_INPUT_H
 
 #include <iostream>
+#include <map>
 
 #include "GLFW/glfw3.h"
+#include "../json.hpp"
 
 namespace KeyInput
 {
@@ -18,6 +20,29 @@ bool justPressed(int key);
 bool pressed(int key);
 
 bool justReleased(int key);
+
+extern const std::map<int, const char *> KEY_NAMES;
+
+const char *getKeyName(int key);
+
+/**
+ * WARNING: very inefficient function..
+ */
+int getKeyByName(const char *keyName);
+
+struct Key
+{
+    int glfwValue = GLFW_KEY_UNKNOWN;
+
+    Key() = default;
+    Key(const int &v) : glfwValue(v) {}
+
+    operator int() const { return glfwValue; }
+};
+
+void to_json(json& j, const Key& v);
+
+void from_json(const json& j, Key& v);
 
 } // namespace KeyInput
 
